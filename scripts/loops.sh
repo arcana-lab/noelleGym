186ccae89c10f4a7eb11fe7c46acbb642c54a451
Simone Campanoni <simo.xan@gmail.com>
2021-11-12 19:24:45 -0600

    Improved a script

--- scripts/loops.sh
+++ scripts/loops.sh
@@ -4,30 +4,58 @@ function generate_loop_results {
   local benchSuite="$1" ;
 
   # Check if the benchmark suite has been compiled
-  currentIVResult="${ivResult}/${benchSuite}" ;
   if ! test -d ${benchSuite}/benchmarks ; then
     return ;
   fi
 
-  pushd ./ ;
+  # Create the output directory
+  currentIVResult="${ivResult}/${benchSuite}" ;
   mkdir -p $currentIVResult ;
+
+  # Collect the data
+  pushd ./ ;
   cd ${benchSuite}/benchmarks ;
   for i in `ls` ; do
     if ! test -d $i ; then
       continue ;
     fi
-    pushd ./ ;
-    cd $i ;
-    if ! test -e ; then
+    if ! test -e ${i}/baseline_with_metadata.bc ; then
       continue ;
     fi
 
-    noelle-loop-stats baseline_with_metadata.bc &> ${currentIVResult}/${i}.txt
+    pushd ./ ;
+    cd $i ;
+    mkdir -p ${currentIVResult}/raw ;
+    if ! test -e ${currentIVResult}/raw/${i}.txt ; then
+      noelle-loop-stats baseline_with_metadata.bc &> ${currentIVResult}/raw/${i}.txt
+    fi
     popd ;
   done
   popd ;
 }
 
+function analyze_results {
+  local benchSuite="$1" ;
+  currentIVResult="${ivResult}/${benchSuite}" ;
+
+  # Check if the benchmark suite has raw data
+  if ! test -d ${currentIVResult}/raw ; then
+    return ;
+  fi
+
+  # Analyze the data
+  pushd ./ ;
+  cd ${currentIVResult}/raw ; 
+  if ! test -e ../induction_variables.txt ; then
+    ${origDir}/scripts/extract_IV.sh > ../induction_variables.txt
+  fi
+  if ! test -e ../invariants.txt ; then
+    ${origDir}/scripts/extract_invariants.sh > ../invariants.txt
+  fi
+  popd ;
+
+  return ;
+}
 
 # Define the directory where we are going to dump the results
 origDir=`pwd` ;
@@ -35,13 +63,17 @@ ivResult="${origDir}/results/current_machine/loops";
 
 # Generate the results for all benchmarks in all benchmark suites
 pushd ./ ;
-cd build ;
+cd all_benchmark_suites/build ;
 for i in `ls` ; do
   if ! test -d $i ; then
     continue ;
   fi
   echo "Benchmark suite $i" ;
 
+  # Generate the raw data
   generate_loop_results $i ;
+
+  # Analyze the raw data
+  analyze_results $i ;
 done
 popd ;
