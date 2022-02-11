58c5d971e0996e7570084b83b04a0675cda5aa97
Simone Campanoni <simo.xan@gmail.com>
2022-02-11 07:50:44 -0600

    Improved the cleaning script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,6 +2,9 @@
 
 rm -f results/current_machine/IR/*/benchmarks/*/*\.ll ;
 rm -f log/condor/*.txt ;
+rm -f log/condor/*.out ;
+rm -f log/condor/*.err ;
+rm -f log/condor/*.log ;
 rm -f *.con ;
 
 find ./results/ -empty -delete ;
