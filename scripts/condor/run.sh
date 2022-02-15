18e3dc11aacf64e1167cae3f7e328710d47f8f16
Simone Campanoni <simo.xan@gmail.com>
2022-02-15 10:57:22 -0600

    Improved the condor support

--- scripts/condor/run.sh
+++ scripts/condor/run.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -e
 
 # Fetch the inputs
-if test $# -lt 1; then
-  echo "USAGE: `basename $0` REPO_DIR" ;
+if test $# -lt 2; then
+  echo "USAGE: `basename $0` REPO_DIR RUN_BASELINE_ONLY" ;
   exit 1;
 fi
 
 # Go to the repository
 cd $1 ;
 
-# Generate and run the binaries
-./bin/runBaseline
+# Check if we need to run only the baseline
+if test $2 == "baseline" ; then
+
+  # Generate and run the binaries for the baseline
+  ./bin/runBaseline ;
+  exit 0;
+fi
+
+# Check if we need to run only the enablers
+if test $2 == "all" ; then
+
+  # Run
+  ./bin/run ;
+  exit 0;
+fi
+
+# Generate and run the binaries for the baseline
+./bin/runTechnique $2 ;
