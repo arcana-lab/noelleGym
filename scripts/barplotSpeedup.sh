58a0b825064719527445dbb3826b5b38e6713ff7
Yian Su <yiansu2018@u.northwestern.edu>
2022-06-12 13:56:56 -0500

    Added feature to generate benchmark speedup plots while running compute_speedup.sh

--- scripts/barplotSpeedup.sh
+++ scripts/barplotSpeedup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+# Fetch the input
+if test $# -lt 2 ; then
+  echo "USAGE: `basename $0` TIME_DIR PLOTS_DIR (OPTIONAL)PRALLELIZATION_TECHNIQUES" ;
+  exit 1;
+fi
+timeDir=$1 ;
+plotsDir=$2 ;
+
 thisFileAbsolutePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" ;
 repoPath="${thisFileAbsolutePath}/.." ;
 
-python3 barplotSpeedup.py ${repoPath}/results/authors_machine/time/PARSEC3/DOALL.txt ${repoPath}/results/authors_machine/time/PARSEC3/HELIX.txt ${repoPath}/results/authors_machine/time/PARSEC3/DSWP.txt ${repoPath}/results/authors_machine/time/PARSEC3/gcc.txt ${repoPath}/results/authors_machine/time/PARSEC3/gcc-par.txt ${repoPath}/results/authors_machine/time/PARSEC3/icc.txt ${repoPath}/results/authors_machine/time/PARSEC3/icc-par.txt ${repoPath}/results/authors_machine/time/MiBench/DOALL.txt ${repoPath}/results/authors_machine/time/MiBench/HELIX.txt ${repoPath}/results/authors_machine/time/MiBench/DSWP.txt ${repoPath}/results/authors_machine/time/MiBench/gcc.txt ${repoPath}/results/authors_machine/time/MiBench/gcc-par.txt ${repoPath}/results/authors_machine/time/MiBench/icc.txt ${repoPath}/results/authors_machine/time/MiBench/icc-par.txt ;
-
-pdfcrop barplotSpeedup.pdf barplotSpeedup.pdf
+python3 ${repoPath}/scripts/barplotSpeedup.py ${timeDir} ${plotsDir} ${@:3} ;
