40035477abfb6de81cc7cb4a5ed719f707c248b9
Yian Su <yiansu2018@u.northwestern.edu>
2022-06-13 12:24:24 -0500

    Added scripts to generate python virtual environment

--- scripts/barplotSpeedup.sh
+++ scripts/barplotSpeedup.sh
@@ -11,4 +11,8 @@ plotsDir=$2 ;
 thisFileAbsolutePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" ;
 repoPath="${thisFileAbsolutePath}/.." ;
 
+# Setup and source the python virtual environment
+source ${repoPath}/scripts/setup_python_virtual_environment.sh ;
+
+# Run the python script to generate plots per benchmark suite
 python3 ${repoPath}/scripts/barplotSpeedup.py ${timeDir} ${plotsDir} ${@:3} ;
