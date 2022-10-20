56dbd883dfb8998a535f08e2a05b4f8e57e46e50
Simone Campanoni <simo.xan@gmail.com>
2022-10-20 13:49:26 -0500

    Improved the cleaning script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,3 +8,19 @@ rm -f log/condor/*.log ;
 rm -f *.con ;
 
 find ./results/ -empty -delete ;
+
+if test -d all_benchmark_suites ; then
+  pushd ./ ;
+  cd all_benchmark_suites ;
+  cd build ;
+  for i in `ls` ; do
+    if ! test -d $i ; then
+      continue ;
+    fi
+    pushd ./ ;
+    cd $i ;
+    make clean ;
+    popd ;
+  done
+  popd ;
+fi
